b26042350820a8cbe048c634bb46f3d8133d7686
sheenam-gupta <sheenam.gupta@fdmgroup.com>
2025-03-19 13:16:11 -0400

    test runner commit

---
+++ FDM_NGF_SeleniumCucumber/src/test/java/com/fdmgroup/testrunner/TestRunner.java
@@ -0,0 +1,26 @@
+package com.fdmgroup.testrunner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features = "src/test/resources/Features", glue = "com.fdmgroup", plugin = { "pretty",
+		"json:Reports/Sprint20/Sprint20_cucumber.json", "junit:Reports/Sprint20/Sprint20_cucumber.junit",
+// "html:Reports/Sprint20/Sprint20_cucumber.html"
+
+}, dryRun = false, publish = true, monochrome = false,
+		// Remember if you are using a logging in method, to change the log in data in
+		// the data folder under "Log In Data"
+		// and use the log in method in the browser utilities class
+		// Place the necessary tag below to run the test
+		// Introduce And User enters their username and password and clicks the log in
+		// button to your cucumber feature file
+		tags = "@RiskAssessor")
+
+public class TestRunner {
+
+}
--- FDM_NGF_SeleniumCucumber/src/test/resources/fdmgroup/testrunner/TestRunner.java
+++ FDM_NGF_SeleniumCucumber/src/test/resources/fdmgroup/testrunner/TestRunner.java
@@ -3,22 +3,24 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
         features = "src/test/resources/Features", glue = "com.fdmgroup",
         plugin = {
             "pretty",
-			"json:Reports/Sprint18/Sprint18_cucumber.json",
-			"junit:Reports/Sprint18/Sprint18_cucumber.junit",
-			"html:Reports/Sprint18/Sprint18_cucumber.html"
-        
+			"json:Reports/Sprint20/Sprint20_cucumber.json",
+			"junit:Reports/Sprint20/Sprint20_cucumber.junit",
+			//"html:Reports/Sprint20/Sprint20_cucumber.html"
+			  //"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+			"com.cucumber.listener.ExtentCucumberFormatter:Reports/Sprint20/Sprint20_cucumber.html"
         }, dryRun = false, publish = true, monochrome = false,
             // Remember if you are using a logging in method, to change the log in data in the data folder under "Log In Data" 
             // and use the log in method in the browser utilities class
             //Place the necessary tag below to run the test
             // Introduce And User enters their username and password and clicks the log in button to your cucumber feature file
-            tags="@MockExam_Loading_Spinner_Error_Message"
+            tags="@RiskAssessor"
 )
 public class TestRunner {
 
